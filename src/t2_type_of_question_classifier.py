28f321fc07899c259bbc77c9680a389c25af0d41
raquel <raquel.perez@bsc.es>
2020-01-03 00:09:52 +0100

    Add preprocessing

--- src/t2_type_of_question_classifier.py
+++ src/t2_type_of_question_classifier.py
@@ -7,21 +7,21 @@ import pandas as pd
 import seaborn as sns
 
 from matplotlib import pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVC
 
 from src.t1_type_of_question_classifier import extract_features, train_model
 
 sns.set(style="darkgrid")
 
-data_analysis_path = '../results/quora_dataset_experiments/'
-os.makedirs(data_analysis_path, exist_ok=True)
+results_path = '../results/t2_experiments/'
 
 
-def split_and_extract_features(df):
+def split_and_extract_features(df, preprocessing):
     x_original = df.loc[df['original_dataset'].values, 'Question']
     y_original = df.loc[df['original_dataset'].values, 'category_id']
 
@@ -36,32 +36,51 @@ def split_and_extract_features(df):
     x_train = np.append(x_train_original.values, x_quora.values)
     y_train = np.append(y_train_original, y_quora)
 
-    features_train, features_test = extract_features(x_train, x_test)
+    features_train, features_test = extract_features(x_train, x_test, preprocessing)
 
     return features_train, features_test, y_train, y_test
 
 
 if __name__ == '__main__':
     df = pd.read_csv('../data/All_Questions.csv', index_col=0)
-    features_train, features_test, y_train, y_test = split_and_extract_features(df)
-    models = [
-        RandomForestClassifier(n_estimators=200, n_jobs=-1),
-        LinearSVC(),
-        MultinomialNB(),
-        LogisticRegression(random_state=0),
+
+    voting_estimators = [
+        ('Random Forest', RandomForestClassifier(n_estimators=200, n_jobs=-1, class_weight='balanced')),
+        ('LinearSVC', LinearSVC(class_weight='balanced')),
+        ('LogisticRegression', LogisticRegression(class_weight='balanced'))
     ]
-    models_names = ['RF', 'LinearSVC', 'Multinomial', 'LogisticRegression']
+
+    models = [
+        RandomForestClassifier(n_estimators=200, n_jobs=-1, class_weight='balanced'),
+        LinearSVC(class_weight='balanced'),
+        MultinomialNB(),
+        LogisticRegression(random_state=0, class_weight='balanced'),
+        VotingClassifier(estimators=voting_estimators),
+        MLPClassifier(hidden_layer_sizes=100)
+    ]
+    models_names = ['Random Forest', 'LinearSVC', 'Multinomial', 'LogisticRegression', 'Voting Classifier',
+                    'MLP default']
+
+    preprocessing = True
 
     accuracies = []
-
-    for model, model_name in zip(models, models_names):
-        print(model_name)
+    with open(os.path.join(results_path, 'quora{}.txt'.format('_preprocessing' if preprocessing else '')), 'w') as f:
         initial_time = time()
-        acc = train_model(model, features_train, features_test, y_train, y_test)
-        accuracies.append(acc)
-        print('Time training {}: '.format(model_name), timedelta(seconds=time() - initial_time))
+        features_train, features_test, y_train, y_test = split_and_extract_features(df, preprocessing)
+        f.write('Time Preprocessing: {}\n '.format(timedelta(seconds=time() - initial_time)))
+        for model, model_name in zip(models, models_names):
+            print(model_name)
+            initial_time = time()
+            f.write('{}\n'.format(model_name))
+            acc, classification_report = train_model(model, features_train, features_test, y_train, y_test)
+            f.write('Classification Report:{}\n'.format(classification_report))
+            accuracies.append(acc)
+            print('Time training {}: '.format(model_name), timedelta(seconds=time() - initial_time))
+            f.write('Time training: {}\n '.format(timedelta(seconds=time() - initial_time)))
+            f.write('\n' + '-' * 80 + '\n\n')
 
     sns.barplot(x=models_names, y=accuracies)
     plt.ylabel('Accuracies')
     plt.ylim(0, 1)
-    plt.savefig(data_analysis_path + 'barplot_quora_' + 'baseline_models_with_stop_words', bbox_inches='tight', dpi=200)
+    plt.savefig(results_path + 'barplot_quora_'.format('preprocessing' if preprocessing else ''), bbox_inches='tight',
+                dpi=200)
