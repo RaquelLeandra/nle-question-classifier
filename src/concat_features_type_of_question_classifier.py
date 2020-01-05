Fix features computation with quora

src/concat_features_type_of_question_classifier.py
@@ -14,7 +14,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.neural_network import MLPClassifier
 
 from src.data_analysis import clean_word
-from src.t1_type_of_question_classifier import train_model
+from src.t1_type_of_question_classifier import train_model, extract_tfidf
 from sklearn.svm import LinearSVC
 
 question_types = ['summary', 'list', 'yesno', 'factoid']
@@ -53,49 +53,8 @@ def get_length_phrase(phrase):
 
 def clean_word(word):
     word = word.lower()
-    without_symbols = re.sub('[^a-zA-Z-_]+', ' ', word)
-    return without_symbols
-
-
-def important_words(questions, max_num_words=100):
-    words_by_class = {}
-    all_the_words = [clean_word(w) for s in
-                     questions['Question'].values for w in s.split()]
-    word_set = set()
-
-    most_common_words_from_all_types = [word for word, word_count in Counter(all_the_words).most_common(max_num_words)]
-    print('Most common words in the whole dataset:\n', most_common_words_from_all_types)
-    df_most_common = pd.DataFrame(0, index=most_common_words_from_all_types, columns=question_types)
-
-    for question_type in question_types:
-        word_list = [clean_word(w) for s in
-                     questions.loc[questions['Type'] == question_type, 'Question'].values for w in s.split() if
-                     clean_word(w) in most_common_words_from_all_types]
-        for word in word_list:
-            # if word not in set(stopwords.words('english')):
-            word_set.add(word)
-        count = Counter(word_list)
-        words = list(count.keys())
-        values = list(count.values())
-
-        df_most_common.loc[words, question_type] = values
-        print('{}:'.format(question_type, len(Counter(word_list))), '\n', Counter(word_list).most_common(max_num_words),
-              '\n')
-        words_by_class[question_type] = word_list
-
-    print(df_most_common)
-    total_occurrences = df_most_common['summary'] + df_most_common['list'] + df_most_common['yesno'] + df_most_common[
-        'factoid']
-    df_important_words = df_most_common[(df_most_common['summary'] >= 0.45 * total_occurrences) |
-                                        (df_most_common['list'] >= 0.45 * total_occurrences) |
-                                        (df_most_common['yesno'] >= 0.45 * total_occurrences) |
-                                        (df_most_common['factoid'] >= 0.45 * total_occurrences) |
-                                        (df_most_common['summary'] <= 0.05 * total_occurrences) |
-                                        (df_most_common['list'] <= 0.05 * total_occurrences) |
-                                        (df_most_common['yesno'] <= 0.05 * total_occurrences) |
-                                        (df_most_common['factoid'] <= 0.05 * total_occurrences)]
-    print('Num of important words:', df_important_words.shape)
-    return df_important_words.index.values
+    word = re.sub(r'[^\w]', '', word)
+    return word
 
 
 def check_word_exists_in(phrase, important_word):
@@ -146,6 +105,8 @@ def split_small_dataset(df):
 
 
 def split_quora(df):
+    df = df.dropna()
+
     x_original = df.loc[df['original_dataset'].values, 'Question']
     y_original = df.loc[df['original_dataset'].values, 'Type']
 
@@ -168,24 +129,10 @@ def split_quora(df):
     return x_train, x_test
 
 
-if __name__ == '__main__':
-    # dataset_name = 'Questions'
-    # questions = pd.read_excel('../data/{}.xlsx'.format(dataset_name))
-    dataset_name = 'whole_quora'
-    questions = pd.read_csv('../data/{}.csv'.format(dataset_name), index_col=0)
-    questions = questions.dropna()
-
+def extract_features(questions_train, questions_test):
+    features = pd.DataFrame()
     features_train = pd.DataFrame()
     features_test = pd.DataFrame()
-    # Shuffle questions dataset
-    if dataset_name == 'Questions':
-        questions_train, questions_test = split_small_dataset(questions)
-    else:
-        questions_train, questions_test = split_quora(questions)
-
-    # First word
-    features_train['first_word'] = questions_train['Question'].apply(get_first_element)
-    features_test['first_word'] = questions_test['Question'].apply(get_first_element)
 
     # Last word
     features_train['last_word'] = questions_train['Question'].apply(get_last_element)
@@ -199,10 +146,6 @@ if __name__ == '__main__':
     features_train['has_question_mark'] = features_train['last_word'].apply(get_question_mark) * 1
     features_test['has_question_mark'] = features_test['last_word'].apply(get_question_mark) * 1
 
-    # Last word without question mark
-    features_train['last_word'] = features_train['last_word'].apply(remove_question_mark)
-    features_test['last_word'] = features_test['last_word'].apply(remove_question_mark)
-
     # Get average length of the dataset
     average_len_train = questions_train['Question'].apply(get_length_phrase).mean()
 
@@ -210,29 +153,37 @@ if __name__ == '__main__':
     features_train['long_phrase'] = questions_train['Question'].apply(get_length_phrase) > average_len_train * 1
     features_test['long_phrase'] = questions_test['Question'].apply(get_length_phrase) > average_len_train * 1
 
-    # iImportant words
-    important_words = important_words(questions_train)
-    for important_word in important_words:
-        features_train[important_word] = questions_train['Question'].apply(check_word_exists_in,
-                                                                           important_word=important_word) * 1
-        features_test[important_word] = questions_test['Question'].apply(check_word_exists_in,
-                                                                         important_word=important_word) * 1
-
-    # # Train first and last words to one hot
-    one_hot_train = pd.get_dummies(features_train['first_word'], prefix='first_word')
-    features_train = features_train.drop(['first_word', 'last_word'], axis=1)
-    features_train.join(one_hot_train)
-
-    # # Test first and last words to one hot
-    one_hot_test = pd.get_dummies(features_test['first_word'], prefix='first_word')
-    features_test = features_test.drop(['first_word', 'last_word'], axis=1)
-    features_test.join(one_hot_test)
+    features_train = features_train.drop(['last_word'], axis=1)
+    features_test = features_test.drop(['last_word'], axis=1)
 
     X_train = features_train.values
     y_train = questions_train['Type'].values
     X_test = features_test.values
     y_test = questions_test['Type'].values
+    return X_train, y_train, X_test, y_test
 
-    results_path = '../results/features_experiments/'
+
+if __name__ == '__main__':
+    # dataset_name = 'Questions'
+    # questions = pd.read_excel('../data/{}.xlsx'.format(dataset_name))
+    dataset_name = 'whole_quora'
+    questions = pd.read_csv('../data/{}.csv'.format(dataset_name), index_col=0)
+    preprocessing = False
+
+    # Shuffle questions dataset
+    if dataset_name == 'Questions':
+        questions_train, questions_test = split_small_dataset(questions)
+    else:
+        questions_train, questions_test = split_quora(questions)
+
+    features_train, y_train, features_test, y_test = extract_features(questions_train, questions_test)
+
+    tf_train, tf_test = extract_tfidf(questions_train['Question'].values, questions_test['Question'].values,
+                                      preprocessing=preprocessing)
+
+    X_train = np.concatenate([features_train, tf_train], axis=1)
+    X_test = np.concatenate([features_test, tf_test], axis=1)
+
+    results_path = '../results/concat_experiments/'
     os.makedirs(results_path, exist_ok=True)
-    run_all_experiments(results_path, X_train, y_train, X_test, y_test, preprocessing=False)
+    run_all_experiments(results_path, X_train, y_train, X_test, y_test, preprocessing=preprocessing)

src/t2_type_of_question_classifier.py
@@ -14,7 +14,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVC
 
-from src.t1_type_of_question_classifier import extract_features, train_model
+from src.t1_type_of_question_classifier import extract_tfidf, train_model
 
 sns.set(style="darkgrid")
 
@@ -36,7 +36,7 @@ def split_and_extract_features(df, preprocessing):
     x_train = np.append(x_train_original.values, x_quora.values)
     y_train = np.append(y_train_original, y_quora)
 
-    features_train, features_test = extract_features(x_train, x_test, preprocessing)
+    features_train, features_test = extract_tfidf(x_train, x_test, preprocessing)
 
     return features_train, features_test, y_train, y_test
 